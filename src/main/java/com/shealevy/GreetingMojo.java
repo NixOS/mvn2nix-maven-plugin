f4445c19a81a3b63b02324a303ccbc7f833cd8f8
Shea Levy <shea@shealevy.com>
2015-06-16 15:08:04 -0400

    Initial commit

---
+++ src/main/java/com/shealevy/GreetingMojo.java
@@ -0,0 +1,14 @@
+package com.shealevy;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "sayhi")
+public class GreetingMojo extends AbstractMojo
+{
+	public void execute() throws MojoExecutionException
+	{
+		getLog().info("Hello, world.");
+	}
+}
