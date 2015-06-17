f482d94765bf0f2073de39ae58dd770fd57cd94f
Shea Levy <shea@shealevy.com>
2015-06-16 20:41:20 -0400

    Calculate the hash of each dependency

--- src/main/java/com/shealevy/Mvn2NixMojo.java
+++ src/main/java/com/shealevy/Mvn2NixMojo.java
@@ -1,6 +1,11 @@
 package com.shealevy;
 
 import java.util.ArrayList;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -10,6 +15,7 @@ import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.metadata.ArtifactRepositoryMetadata;
+
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.collection.CollectRequest;
@@ -22,7 +28,9 @@ import org.eclipse.aether.graph.Exclusion;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
 
-@Mojo(name = "sayhi")
+import org.apache.commons.codec.binary.Hex;
+
+@Mojo(name = "mvn2nix")
 public class Mvn2NixMojo extends AbstractMojo
 {
 	@Component
@@ -71,13 +79,31 @@ public class Mvn2NixMojo extends AbstractMojo
 					"Resolving dependencies",
 					e);
 		}
+		ByteBuffer buf = ByteBuffer.allocateDirect(512 * 125);
 		for (ArtifactResult res : results.getArtifactResults()) {
 			Artifact art = res.getArtifact();
-			if (art != null && art.getFile() != null) {
-				getLog().info(art.getFile().toString());
-			} else {
-				getLog().info("Not resolved");
+			MessageDigest md;
+			try {
+				md = MessageDigest.getInstance("SHA-256");
+			} catch (NoSuchAlgorithmException e) {
+				throw new MojoExecutionException(
+						"Creating SHA-256 hash",
+						e);
 			}
+			try (FileChannel fc = FileChannel.open(
+						art.getFile().toPath())) {
+				while (fc.read(buf) != -1) {
+					buf.flip();
+					md.update(buf);
+					buf.clear();
+				}
+			} catch (IOException e) {
+				throw new MojoExecutionException(
+						"Reading artifact",
+						e);
+			}
+			getLog().info(art.getFile().toString());
+			getLog().info(new String(Hex.encodeHex(md.digest())));
 		}
 	}
 }
