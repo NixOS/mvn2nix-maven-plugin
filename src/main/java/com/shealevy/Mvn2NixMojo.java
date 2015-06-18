26a7e0f9d3b42f731a75d4781294a2c7ff64c1f3
Shea Levy <shea@shealevy.com>
2015-06-18 16:35:02 -0400

    Refactor, get plugin info, output an object not a list

--- src/main/java/com/shealevy/Mvn2NixMojo.java
+++ src/main/java/com/shealevy/Mvn2NixMojo.java
@@ -2,6 +2,8 @@ package com.shealevy;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.io.IOException;
@@ -56,35 +58,49 @@ public class Mvn2NixMojo extends AbstractMojo
 	@Parameter(defaultValue="${repositorySystemSession}", readonly=true)
 	private RepositorySystemSession repoSession;
 
-	public void execute() throws MojoExecutionException
+	private Dependency convertMavenDependency(
+			org.apache.maven.model.Dependency mvnDep)
 	{
-		ArrayList<Dependency> deps = new ArrayList<Dependency>();
-		for (org.apache.maven.model.Dependency mvnDep :
-				project.getDependencies()) {
-			Artifact art = new DefaultArtifact(mvnDep.getGroupId(),
-					mvnDep.getArtifactId(),
-					mvnDep.getClassifier(),
-					mvnDep.getType(),
-					mvnDep.getVersion());
-			String scope = mvnDep.getScope();
-			Boolean opt = new Boolean(mvnDep.isOptional());
-			ArrayList<Exclusion> excls = new ArrayList<Exclusion>();
-			for (org.apache.maven.model.Exclusion mvnExcl :
-					mvnDep.getExclusions()) {
-				Exclusion excl = new Exclusion(
-						mvnExcl.getGroupId(),
-						mvnExcl.getArtifactId(),
-						null,
-						null);
-				excls.add(excl);
-			}
-			Dependency dep = new Dependency(art, scope, opt, excls);
-			deps.add(dep);
+		Artifact art = new DefaultArtifact(mvnDep.getGroupId(),
+				mvnDep.getArtifactId(),
+				mvnDep.getClassifier(),
+				mvnDep.getType(),
+				mvnDep.getVersion());
+		String scope = mvnDep.getScope();
+		Boolean opt = new Boolean(mvnDep.isOptional());
+		ArrayList<Exclusion> excls = new ArrayList<Exclusion>();
+		for (org.apache.maven.model.Exclusion mvnExcl :
+				mvnDep.getExclusions()) {
+			Exclusion excl = new Exclusion(
+					mvnExcl.getGroupId(),
+					mvnExcl.getArtifactId(),
+					null,
+					null);
+			excls.add(excl);
 		}
-		List<RemoteRepository> projectRepos =
-			project.getRemoteProjectRepositories();
-		CollectRequest cr = new CollectRequest((Dependency) null, deps,
-				projectRepos);
+		return new Dependency(art, scope, opt, excls);
+	}
+
+	private Dependency convertMavenArtifact(
+			org.apache.maven.artifact.Artifact mvnArt)
+	{
+		Artifact art = new DefaultArtifact(mvnArt.getGroupId(),
+				mvnArt.getArtifactId(),
+				mvnArt.getClassifier(),
+				"jar",
+				mvnArt.getVersion());
+		String scope = mvnArt.getScope();
+		Boolean opt = new Boolean(mvnArt.isOptional());
+		return new Dependency(art, scope, opt);
+	}
+
+	private void emitDependencies(Dependency rootDep,
+		List<RemoteRepository> repos,
+		ByteBuffer buf,
+		JsonGenerator gen,
+		Set seenSet) throws MojoExecutionException
+	{
+		CollectRequest cr = new CollectRequest(rootDep, repos);
 		DependencyRequest dr = new DependencyRequest(cr, null);
 		DependencyResult results;
 		try {
@@ -95,117 +111,148 @@ public class Mvn2NixMojo extends AbstractMojo
 					"Resolving dependencies",
 					e);
 		}
+		for (ArtifactResult res :
+				results.getArtifactResults()) {
+			Artifact art = res.getArtifact();
+			String key = art.getGroupId() + ":" +
+				art.getArtifactId() + ":" +
+				art.getVersion();
+			if (!seenSet.add(key)) {
+				continue;
+			}
+			gen.writeStartObject(key);
+
+			gen.writeStartObject("artifact");
+			gen.write("artifact-id", art.getArtifactId());
+			gen.write("base-version", art.getBaseVersion());
+			gen.write("classifier", art.getClassifier());
+			gen.write("extension", art.getExtension());
+			gen.write("group-id", art.getGroupId());
+			gen.write("version", art.getVersion());
+			gen.write("snapshot", art.isSnapshot());
+			gen.writeEnd();
+
+			RemoteRepository repo =
+				(RemoteRepository) res.getRepository();
+			gen.writeStartObject("repository");
+			gen.write(
+				"authenticated",
+				repo.getAuthentication() != null);
+			gen.write("content-type",
+					repo.getContentType());
+			gen.write("id", repo.getId());
+			gen.write("url", repo.getUrl());
+			gen.writeEnd();
+
+			DependencyNode node = res.getRequest()
+				.getDependencyNode();
+			if (node != null) {
+				Dependency dep = node.getDependency();
+				gen.writeStartObject("dependency");
+				gen.write("scope", dep.getScope());
+				gen.write("optional", dep.isOptional());
+				gen.writeStartArray("exclusions");
+				for (Exclusion excl :
+						dep.getExclusions()) {
+					gen.writeStartObject();
+					gen.write(
+						"artifact-id",
+						excl.getArtifactId());
+					gen.write(
+						"classifier",
+						excl.getClassifier());
+					gen.write(
+						"extension",
+						excl.getExtension());
+					gen.write(
+						"group-id",
+						excl.getGroupId());
+					gen.writeEnd();
+				}
+				gen.writeEnd();
+				gen.writeEnd();
+			}
+
+			MessageDigest md;
+			try {
+				md = MessageDigest.getInstance(
+						"SHA-256");
+			} catch (NoSuchAlgorithmException e) {
+				throw new MojoExecutionException(
+						"Creating SHA-256 hash",
+						e);
+			}
+			try (FileChannel fc = FileChannel.open(
+					art.getFile().toPath())) {
+				while (fc.read(buf) != -1) {
+					buf.flip();
+					md.update(buf);
+					buf.clear();
+				}
+			} catch (IOException e) {
+				throw new MojoExecutionException(
+						"Reading artifact",
+						e);
+			}
+			gen.write("sha256", Hex.encodeHexString(
+						md.digest()));
+
+			RepositoryLayout layout;
+			try {
+				layout = layoutProvider
+					.newRepositoryLayout(
+						repoSession,
+						repo);
+			} catch (NoRepositoryLayoutException e) {
+				throw new MojoExecutionException(
+					"Getting repository layout",
+					e);
+			}
+
+			URI abs;
+			try {
+				/* Intentionally not using URI.resolve,
+				 * it drops any path component in the
+				 * original URL.
+				 */
+				abs = new URI(repo.getUrl() + "/" +
+					layout.getLocation(art, false));
+			} catch (URISyntaxException e) {
+				throw new MojoExecutionException(
+					"Parsing repository URI",
+					e);
+			}
+			gen.write("url", abs.toString());
+			gen.writeEnd();
+		}
+	}
+
+	public void execute() throws MojoExecutionException
+	{
+		ArrayList<Dependency> deps = new ArrayList<Dependency>();
 		ByteBuffer buf = ByteBuffer.allocateDirect(512 * 125);
+		Set<String> seenSet = new HashSet<String>();
 		try (JsonGenerator gen = Json.createGenerator(
 					new FileOutputStream("deps.json"))) {
-			gen.writeStartArray();
-			for (ArtifactResult res :
-					results.getArtifactResults()) {
-				gen.writeStartObject();
-
-				Artifact art = res.getArtifact();
-				gen.writeStartObject("artifact");
-				gen.write("artifact-id", art.getArtifactId());
-				gen.write("base-version", art.getBaseVersion());
-				gen.write("classifier", art.getClassifier());
-				gen.write("extension", art.getExtension());
-				gen.write("group-id", art.getGroupId());
-				gen.write("version", art.getVersion());
-				gen.write("snapshot", art.isSnapshot());
-				gen.writeEnd();
-
-				RemoteRepository repo =
-					(RemoteRepository) res.getRepository();
-				gen.writeStartObject("repository");
-				gen.write(
-					"authenticated",
-					repo.getAuthentication() != null);
-				gen.write("content-type",
-						repo.getContentType());
-				gen.write("id", repo.getId());
-				gen.write("url", repo.getUrl());
-				gen.writeEnd();
-
-				DependencyNode node = res.getRequest()
-					.getDependencyNode();
-				if (node != null) {
-					Dependency dep = node.getDependency();
-					gen.writeStartObject("dependency");
-					gen.write("scope", dep.getScope());
-					gen.write("optional", dep.isOptional());
-					gen.writeStartArray("exclusions");
-					for (Exclusion excl :
-							dep.getExclusions()) {
-						gen.writeStartObject();
-						gen.write(
-							"artifact-id",
-							excl.getArtifactId());
-						gen.write(
-							"classifier",
-							excl.getClassifier());
-						gen.write(
-							"extension",
-							excl.getExtension());
-						gen.write(
-							"group-id",
-							excl.getGroupId());
-						gen.writeEnd();
-					}
-					gen.writeEnd();
-					gen.writeEnd();
-				}
-
-				MessageDigest md;
-				try {
-					md = MessageDigest.getInstance(
-							"SHA-256");
-				} catch (NoSuchAlgorithmException e) {
-					throw new MojoExecutionException(
-							"Creating SHA-256 hash",
-							e);
-				}
-				try (FileChannel fc = FileChannel.open(
-						art.getFile().toPath())) {
-					while (fc.read(buf) != -1) {
-						buf.flip();
-						md.update(buf);
-						buf.clear();
-					}
-				} catch (IOException e) {
-					throw new MojoExecutionException(
-							"Reading artifact",
-							e);
-				}
-				gen.write("sha256", Hex.encodeHexString(
-							md.digest()));
-
-				RepositoryLayout layout;
-				try {
-					layout = layoutProvider
-						.newRepositoryLayout(
-							repoSession,
-							repo);
-				} catch (NoRepositoryLayoutException e) {
-					throw new MojoExecutionException(
-						"Getting repository layout",
-						e);
-				}
-
-				URI abs;
-				try {
-					/* Intentionally not using URI.resolve,
-					 * it drops any path component in the
-					 * original URL.
-					 */
-					abs = new URI(repo.getUrl() + "/" +
-						layout.getLocation(art, false));
-				} catch (URISyntaxException e) {
-					throw new MojoExecutionException(
-						"Parsing repository URI",
-						e);
-				}
-				gen.write("url", abs.toString());
-				gen.writeEnd();
+			gen.writeStartObject();
+			List<RemoteRepository> repos =
+				project.getRemoteProjectRepositories();
+			for (org.apache.maven.model.Dependency mvnDep :
+					project.getDependencies()) {
+				emitDependencies(convertMavenDependency(mvnDep),
+					repos,
+					buf,
+					gen,
+					seenSet);
+			}
+			repos = project.getRemotePluginRepositories();
+			for (org.apache.maven.artifact.Artifact mvnArt :
+					project.getPluginArtifacts()) {
+				emitDependencies(convertMavenArtifact(mvnArt),
+					repos,
+					buf,
+					gen,
+					seenSet);
 			}
 			gen.writeEnd();
 		} catch (FileNotFoundException e) {
