91f396985a954b132a541d13006156b3614571ab
Shea Levy <shea@shealevy.com>
2015-06-21 21:43:35 -0400

    Javadoc

--- src/main/java/com/shealevy/Mvn2NixMojo.java
+++ src/main/java/com/shealevy/Mvn2NixMojo.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2015 Shea Levy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.shealevy;
 
 import java.util.List;
@@ -41,6 +64,12 @@ import org.eclipse.aether.spi.connector.layout.RepositoryLayoutProvider;
 import org.eclipse.aether.spi.connector.layout.RepositoryLayout;
 import org.eclipse.aether.transfer.NoRepositoryLayoutException;
 
+/**
+ * A Mojo to generate JSON for use with nix's Maven repository generation
+ * functions
+ *
+ * @author Shea Levy
+ */
 @Mojo(name = "mvn2nix")
 public class Mvn2NixMojo extends AbstractMojo
 {
@@ -345,6 +374,7 @@ public class Mvn2NixMojo extends AbstractMojo
 		}
 	}
 
+	@Override
 	public void execute() throws MojoExecutionException
 	{
 		repoSession = new DefaultRepositorySystemSession(repoSession);
